42482052c4c30ed02f72405300594451ff12c5ff
alienbrainfarm <99015577+alienbrainfarm@users.noreply.github.com>
2023-03-03 11:25:31 +0100

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -5,6 +5,7 @@ def lambda_handler(event, context):
     Args:
         event: trigger event dict
         context: lambda methods and properties
+        
     Returns:
         string: greeting response
     """
